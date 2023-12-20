b28deda0e56866b2344c0a164ffed95f1105fe63
Charles Keating <Charles.Keating@udri.udayton.edu>
2023-12-20 11:18:53 -0500

    feat(ops): Added a new feature to perform square of a sum
    Also broke the release workflow to test out repairing releases after a
    semantic release update

--- test/operations/test_sum.py
+++ test/operations/test_sum.py
@@ -5,3 +5,7 @@ from py_hello_world_keatincf.operations import sum
 
 def test_sum_provides_2x2_ones_matrix():
     assert np.array_equal(sum.sum(), np.ones(shape=[2, 2]))
+
+
+def test_squared_sum_provides_2x2_ones_matrix():
+    assert np.array_equal(sum.squared_sum(), np.ones(shape=[2, 2]))
