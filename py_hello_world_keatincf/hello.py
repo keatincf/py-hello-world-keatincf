6d037f7078e04bc536f43a11168d98c055c71c60
Charles Keating <Charles.Keating@udri.udayton.edu>
2023-12-20 14:20:20 -0500

    fix(main): fixed issue with main not calling new feature

--- py_hello_world_keatincf/hello.py
+++ py_hello_world_keatincf/hello.py
@@ -6,6 +6,7 @@ def main():
     Main functionality for Hello World.  Calculates some sums and prints out the results along with a Hello World message
     """
     print(f"Hello World!\n{sum.sum()}")
+    print(f"Hello World!\n{sum.squared_sum()}")
 
 
 if __name__ == "__main__":
